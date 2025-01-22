a8c50808505efec9199d7c225543327874d24e7f
Imdadul Haque <45633928+imdadulhaque1@users.noreply.github.com>
2025-01-22 15:41:28 +0600

    Room Details Post, fetch, udpate and delete api done

---
+++ snowtexDormitoryApi/DTOs/admin/basicSetup/roomManagementsDto/roomDetails/RoomDetailsPutRequestDto.cs
@@ -0,0 +1,30 @@
+﻿namespace snowtexDormitoryApi.DTOs.admin.basicSetup.roomManagementsDto.roomDetails
+{
+    public class RoomDetailsPutRequestDto
+    {
+        //public int roomId { get; set; }
+        //public int floorId { get; set; }
+        //public int buildingId { get; set; }
+        //public int roomDetailsId { get; set; }
+        public required string roomDimension { get; set; }
+
+        // Room side (1=east, 2=west, 3=north, 4=south)
+        public required int roomSideId { get; set; }
+
+        // 1=Attached Belconi, 2=No Belconi
+        public required int roomBelconiId { get; set; }
+
+        // 1=yes, 2=no
+        public required int attachedBathroomId { get; set; }
+        public required List<int> commonFeatures { get; set; }
+        public required List<int> availableFurnitures { get; set; }
+        public required List<int> bedSpecification { get; set; }
+        public required List<int> bathroomSpecification { get; set; }
+
+        // Room image URLs
+        public List<string>? roomImages { get; set; }
+        public bool? isActive { get; set; } = true;
+        public int? updatedBy { get; set; }
+        public DateTime? updatedTime { get; set; }
+    }
+}
--- snowtexDormitoryApi/obj/Debug/net8.0/snowtexDormitoryApi.AssemblyInfo.cs
+++ snowtexDormitoryApi/obj/Debug/net8.0/snowtexDormitoryApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("snowtexDormitoryApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb7f0acc2a3166b77596ea9de441c553f0cca6c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b629517ae6c011e0c6609b67d04309ac47d9b2a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("snowtexDormitoryApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("snowtexDormitoryApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
